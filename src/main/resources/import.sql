9c7e15a2fa4eb5d2dcdd0422dde1d0e566231f50
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2024-09-10 22:33:23 -0300

    Seed Notification

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -44,3 +44,7 @@ INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, du
 
 INSERT INTO  tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
 INSERT INTO  tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Primeiro feedback da tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2020-09-25T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Segundo feedback: favor revisar', TIMESTAMP WITH TIME ZONE '2020-10-25T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Terceiro feedback de tarefa: favor revisar', TIMESTAMP WITH TIME ZONE '2020-11-25T13:00:00Z', true, '/offers/1/resource/1/sections/1', 1);
